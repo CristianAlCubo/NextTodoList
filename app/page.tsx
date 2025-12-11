f3190bd7c81e65ecce1cd62f36028aa76a992e72
Cristian Cubillos <ccubillosreyes1@gmail.com>
2025-12-11 16:41:28 -0500

    feat: Add change 001

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,12 @@
 const page = () => {
 
   return (
-    <h1 className="text-9xl font-bold text-center mt-50">
-      NextJS + GithubActions
-    </h1>
+    <>
+      <h1 className="text-9xl font-bold text-center mt-50">
+        NextJS + GithubActions
+      </h1>
+      <span className="block text-center text-2xl font-bold text-green-500">Change 001</span>
+    </>
   )
 }
 
